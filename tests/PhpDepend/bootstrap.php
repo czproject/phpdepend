b033ec492065e8c8d2400e6e69d03fef4d9cecc5
Jan Pecha <janpecha@email.cz>
2021-04-04 11:43:01 +0200

    Added Makefile

--- tests/PhpDepend/bootstrap.php
+++ tests/PhpDepend/bootstrap.php
@@ -1,8 +1,3 @@
 <?php
 require __DIR__ . '/../../vendor/autoload.php';
 Tester\Environment::setup();
-
-if (extension_loaded('xdebug'))
-{
-	Tester\CodeCoverage\Collector::start(__DIR__ . '/../coverage.dat');
-}
